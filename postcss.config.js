f49dcbbd5e4afa7c28056196d40368259223bf99
Bernardo António Borda d'Água <berna.agua@gmail.com>
2021-04-14 17:58:30 +0100

    landing page started

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,24 @@
+const cssnano = require("cssnano");
+
 module.exports = {
-    plugins: [
-        require("postcss-import")(),
-        require("postcss-url")(),
-        require('postcss-preset-env')({ stage: 0 }),
-        // require("flex-gap-polyfill"),
-        require("autoprefixer")({}),//overrideBrowserslist: ["> 0.5% in US", "Safari > 9"]
-        require('cssnano')({
-            preset: 'advanced',
-        }),
-    ]
+  plugins: [
+    require("postcss-import")(),
+    require("postcss-url")(),
+    require("postcss-preset-env")({ stage: 0 }),
+    // require("flex-gap-polyfill"),
+    require("autoprefixer")({}),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        "./themes/**/layouts/**/*.html",
+        "./themes/**/archetypes/**/*.html",
+        "./layouts/**/*.html",
+        "./archetypes/**/*.md",
+        "./content/**/*.md",
+      ],
+      css: ["./themes/**/assets/css/**/*.css", "./assets/css/**/*.css"],
+    }),
+    require("cssnano")({
+      preset: "advanced",
+    }),
+  ],
 };
